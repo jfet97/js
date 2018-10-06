a02cd8c5b402154b8db9958631ea45aa23cc5ed2
Andrea Simone Costa <jfet97@users.noreply.github.com>
2018-10-06 11:56:33 +0200

    Create weird.js

---
+++ indovinelli-JavaScript_ITA-telegram/weird.js
@@ -0,0 +1,37 @@
+if(weird == 10 && weird == 20 && weird == 30) {
+  // entra in questo blocco
+}
+
+/*
+  soluzione 1: 
+*/
+
+Object.defineProperty(this || window || global, 'weird', {
+  get: function() {
+        let counter = 0;
+        return function() {
+          return counter += 10;
+        }
+      }(),
+});
+
+/*
+  soluzione 2:
+*/
+
+let weird = [10, 20, 30];
+weird.join = weird.shift; // toString() richiama join()
+
+
+/*
+  soluzione 3:
+*/
+
+let weird = {
+  valueOf: function() {
+            let counter = 0;
+            return function() {
+              return counter += 10;
+            }
+          }(),
+}
